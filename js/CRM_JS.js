9da30f947190bd032dc9f4f0a925f3f006b2e1a4
krio2014 <adamevans385@msn.com>
2017-08-07 17:00:53 +0100

    Added some jquery to hide parts of the main panel
    Added some jquery to hide parts of the main panel
    Thi allows the user to click the heading to hide the fields that appear.
    this makes it easier for smaller screens.

---
+++ js/CRM_JS.js
@@ -0,0 +1,13 @@
+// function tog(){
+//                
+//                alert('Diagnostic - gets to the tog script');
+//                        $('#sideBarTog').show(); 
+// }
+//    function untog(){
+//        $('#sideBarTog').hide();
+//    }
+    
+    function tog(id){
+//		alert('Diagnostic - gets to the tog script - ' + id);
+        $(id).toggle();
+    }
